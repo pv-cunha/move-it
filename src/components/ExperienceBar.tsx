caf27a051a493c496c1830f587d947d9f8c09453
Paulo Victor <pvcunha05@gmail.com>
2021-02-23 12:24:00 -0300

    Module CSS

--- src/components/ExperienceBar.tsx
+++ src/components/ExperienceBar.tsx
@@ -1,11 +1,13 @@
+import styles from '../styles/components/ExperienceBar.module.css'
+
 export const ExperienceBar = () => {
   return (
-    <header className="experienceBar">
+    <header className={styles.experienceBar}>
       <span>0 xp</span>
 
       <div>
         <div style={{width: '50%'}}></div>
-        <span className="current-experience" style={{left: '50%'}}>300 xp</span>
+        <span className={styles.currentExperience} style={{left: '50%'}}>300 xp</span>
       </div>
 
       <span>600 xp</span>
